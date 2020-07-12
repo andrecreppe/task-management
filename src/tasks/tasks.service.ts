7d8f94b25796890ce358e60fbed68e4c9e884766
andrecreppe <andre.creppe@uol.com.br>
2020-07-11 21:36:28 -0300

    cleanup

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
-import { v1 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
