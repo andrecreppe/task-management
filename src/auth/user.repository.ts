064e07f7d12c85efe9d84a5bdce8ce069dd339fa
andrecreppe <andre.creppe@uol.com.br>
2020-07-14 17:09:27 -0300

    Security and validation for authentication
    New security and health features (hash and data filters)

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,9 @@
-import { Repository, EntityRepository } from 'typeorm';
+import { Repository, EntityRepository, Column } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+
+import * as bycript from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,8 +12,34 @@ export class UserRepository extends Repository<User> {
 
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bycript.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
 
-    await user.save();
+    try {
+      await user.save();
+    } catch(error) {
+      console.log(error.code) //- get the query error code
+      if(error.code === 'ER_DUP_ENTRY') { //duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+
+    const user = await this.findOne({ username });
+
+    if(user && await user.validatePassword(password)) {
+      return user.username;
+    } else {
+      return null;
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bycript.hash(password, salt);
   }
 }
