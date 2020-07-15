cff25a22f6997ca71260459e52ac8a77d3b7c1a4
andrecreppe <andre.creppe@uol.com.br>
2020-07-15 11:38:53 -0300

    [CORRECTION] JWT modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,9 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
-    PassportModule.register({
-      defaultStrategy: 'jwt'
-    }),
-
-    JwtModule.register({
-      secret: 'topSecret51', //change to a more elaborated secret
-      signOptions: {
-        expiresIn: 3600, //seconds
-      }
-    }),
-    
     TypeOrmModule.forRoot(typeOrmConfig),
 
     TasksModule,
