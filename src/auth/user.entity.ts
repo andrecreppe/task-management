83374a67b48ce71616c967d1b3fd896d8e74b7ce
andrecreppe <andre.creppe@uol.com.br>
2020-07-16 16:44:23 -0300

    Task ownership - create linking

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,5 +1,6 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, OneToMany } from 'typeorm';
 import * as bycript from 'bcrypt';
+import { Task } from '../tasks/task.entity';
 
 @Entity()
 @Unique(['username'])
@@ -15,6 +16,10 @@ export class User extends BaseEntity {
 
   @Column()
   salt: string;
+
+  @OneToMany(type => Task, task => task.user, { eager: true })
+  //eager true = when retriving user, list of tasks is accesible 
+  tasks: Task[];
 
   async validatePassword(password: string): Promise<boolean> {
     const hash = await bycript.hash(password, this.salt);
